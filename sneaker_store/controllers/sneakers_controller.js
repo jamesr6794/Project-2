5b77ffc6b6f53696517fbbed83904b3913fc43cb
James Rodriguez <JamesR94@macbook-pro.myfiosgateway.com>
2021-03-04 19:51:44 -0500

    pictures working

--- sneaker_store/controllers/sneakers_controller.js
+++ sneaker_store/controllers/sneakers_controller.js
@@ -37,6 +37,102 @@ store.get('/seed', (req,res) => {
             price: 100,
             size: 10,
             addToCart: true
+          }, {
+            img : 'https://stockx-360.imgix.net/Air-Jordan-1-Retro-High-Black-Game-Royal-Toe/Images/Air-Jordan-1-Retro-High-Black-Game-Royal-Toe/Lv2/img02.jpg?auto=format,compress&w=559&q=90&dpr=2&updated_at=1606322574',
+            brand: 'Jordan',
+            name: 'Royal Toe 1',
+            color: 'Royal Blue, White',
+            price: 190,
+            size: 10,
+            addToCart: true
+          }, {
+            img : 'https://cdn.flightclub.com/TEMPLATE/012120/1.jpg',
+            brand: 'Jordan',
+            name: 'Oreo 4',
+            color: 'Black, Grey',
+            price: 220,
+            size: 9.5,
+            addToCart: true
+          }, {
+            img : 'https://cdn.flightclub.com/TEMPLATE/011369/1.jpg',
+            brand: 'Jordan',
+            name: 'Cardinal 7',
+            color: 'White, Red',
+            price: 140,
+            size: 9,
+            addToCart: true
+          }, {
+            img : 'https://cdn.flightclub.com/TEMPLATE/011564/1.jpg',
+            brand: 'Jordan',
+            name: 'Playoff 12',
+            color: 'Black, White',
+            price: 140,
+            size: 9.5,
+            addToCart: true
+          }, {
+            img : 'https://stockx-360.imgix.net/adidas-Yeezy-Boost-350-V2-Core-Black-White/Images/adidas-Yeezy-Boost-350-V2-Core-Black-White/Lv2/img01.jpg?auto=format,compress&w=559&q=90&dpr=2&updated_at=1607647672',
+            brand: 'Adidas',
+            name: 'Yeezy Boost 350 Oreo',
+            color: 'Black, White',
+            price: 200,
+            size: 9.5,
+            addToCart: true
+          }, {
+            img : 'https://assets.adidas.com/images/w_600,f_auto,q_auto/822b784aec17404ba4d9ab6e00fd2e7e_9366/Nite_Jogger_Shoes_Grey_EG8849_01_standard.jpg',
+            brand: 'Adidas',
+            name: 'Nite Jogger',
+            color: 'White',
+            price: 80,
+            size: 9.5,
+            addToCart: true
+          }, {
+            img : 'https://stockx-360.imgix.net/adidas-Yeezy-Boost-350-V2-Beluga-2pt0/Images/adidas-Yeezy-Boost-350-V2-Beluga-2pt0/Lv2/img01.jpg?auto=format,compress&w=559&q=90&dpr=2&updated_at=1606322713',
+            brand: 'Adidas',
+            name: 'Yeezy 350 Beluga',
+            color: 'Grey, Orange',
+            price: 200,
+            size: 9,
+            addToCart: true
+          }, {
+            img : 'https://images.stockx.com/images/Adidas-NMD-HU-Pharrell-Williams-Yellow-Product.jpg?fit=fill&bg=FFFFFF&w=300&h=214&auto=format,compress&q=90&dpr=2&trim=color&updated_at=1606321318',
+            brand: 'Adidas',
+            name: 'Human Race',
+            color: 'Yellow, Black',
+            price: 200,
+            size: 9.5,
+            addToCart: true
+          }, {
+            img : 'https://images.journeys.com/images/products/1_457518_ZM.JPG',
+            brand: 'Vans',
+            name: 'Old Skool Checkerboard',
+            color: 'Black, White',
+            price: 50,
+            size: 9.5,
+            addToCart: true
+          }, {
+            img : 'https://cdn.shopify.com/s/files/1/0248/7857/9748/products/rd_vans-half-cab-black_835x.jpg?v=1579727028',
+            brand: 'Vans',
+            name: 'Half Cab',
+            color: 'Black, White',
+            price: 60,
+            size: 9.5,
+            addToCart: true
+          }, {
+            img : '/images/chukkalow.jpg',
+            brand: 'Vans',
+            name: 'Chukka Low',
+            color: 'Black, White',
+            price: 40,
+            size: 9,
+            addToCart: true
+          }, {
+            img : 'https://www.pacsun.com/dw/image/v2/AAJE_PRD/on/demandware.static/-/Sites-pacsun_storefront_catalog/default/dw878b16cc/product_images/0530037915001NEW_00_359.jpg',
+            brand: 'Vans',
+            name: 'Sk8 - Hi',
+            color: 'Black, White',
+            price: 70,
+            size: 9.5,
+            addToCart: true
           }
     ], (error, allSneakers) => {
         res.redirect('/store')
